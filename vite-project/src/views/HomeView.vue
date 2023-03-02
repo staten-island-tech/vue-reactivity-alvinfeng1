40b9e08518a671cd05ce7a006b7f9ada47d33741
Alvin Feng <113121131+alvinfeng1@users.noreply.github.com>
2023-03-02 09:55:08 -0500

    f

---
+++ vite-project/src/views/HomeView.vue
@@ -0,0 +1,16 @@
+<template>
+  <h1>{{ student }}</h1>
+  <ul>
+    <li>v-for="animal in animals" :key"animal">{{ animal }}</li>
+  </ul>
+</template>
+<script>
+export default  {
+  data() {
+    return {
+      student: "jason",
+      animals: ["pig", "cow", "chicken"]
+    };
+  };
+};
+</script>
