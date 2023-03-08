51887e9ddd1402af7fa075e59af649bb124afe0f
Alvin Feng <113121131+alvinfeng1@users.noreply.github.com>
2023-03-08 10:14:51 -0500

    g

---
+++ vite-project/src/components/shoppingCart.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="box">
+    <h1>Shopping cart</h1>
+    <p>food</p>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+.box {
+  background-color: black;
+  height: 500px;
+  color: white;
+}
+</style>
--- vite-project/src/views/HomeView.vue
+++ vite-project/src/views/HomeView.vue
@@ -1,9 +1,13 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+// import TheWelcome from "../components/TheWelcome.vue";
+import shoppingCart from "../components/shoppingCart.vue";
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <!-- <TheWelcome /> -->
+    <shoppingCart />
   </main>
 </template>
+
+<style></style>
