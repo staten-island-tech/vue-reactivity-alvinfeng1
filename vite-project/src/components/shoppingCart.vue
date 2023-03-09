3ef8300545d7f8f1a7522f83c122be4db7df47af
Alvin Feng <113121131+alvinfeng1@users.noreply.github.com>
2023-03-09 10:15:38 -0500

    f

--- vite-project/src/components/shoppingCart.vue
+++ vite-project/src/components/shoppingCart.vue
@@ -14,5 +14,6 @@ export default {};
   background-color: black;
   height: 500px;
   color: white;
+  text-align: center;
 }
 </style>
--- vite-project/src/views/HomeView.vue
+++ vite-project/src/views/HomeView.vue
@@ -1,13 +1,25 @@
-<script setup>
-// import TheWelcome from "../components/TheWelcome.vue";
-import shoppingCart from "../components/shoppingCart.vue";
-</script>
-
 <template>
-  <main>
-    <!-- <TheWelcome /> -->
-    <shoppingCart />
-  </main>
+  <button @click="show = !show">Toggle</button>
+  <Transition duration="550" class="nested">
+    <div v-if="show" class="outer">
+      <div class="inner">Hello</div>
+    </div>
+  </Transition>
 </template>
 
-<style></style>
+<script>
+export default {};
+</script>
+
+<style scoped>
+.nested .inner,
+.nested .inner {
+  transition: all 0.3s ease-in-out;
+}
+
+.nested .inner,
+.nested .inner {
+  transform: translateX(30px);
+  opacity: 0;
+}
+</style>
