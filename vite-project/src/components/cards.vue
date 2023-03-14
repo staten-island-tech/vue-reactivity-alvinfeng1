9af24ca049009c2f6b4c4680421a8f28af1d9e31
Alvin Feng <113121131+alvinfeng1@users.noreply.github.com>
2023-03-14 10:21:32 -0400

    c

--- vite-project/src/components/cards.vue
+++ vite-project/src/components/cards.vue
@@ -9,12 +9,66 @@ export default {
       food: "",
       newfood: [
         {
-          choclateprice: 9.99,
-          choclateimg: dfasg,
+          id: 1,
+          name: "Hersheys Organic Milk Chocolate Candy",
+          img: (src = ""),
+          price: 3,
         },
-        {},
-        {},
-        {},
+        {
+          id: 2,
+          name: "Reeses Peanut Butter Cups",
+          img: (src = ""),
+          price: 2,
+        },
+        {
+          id: 3,
+          name: "Skittles",
+          img: (src = ""),
+          price: 1,
+        },
+        {
+          id: 4,
+          name: "Jolly Rancher Hard Candy, Assorted",
+          img: (src = ""),
+          price: 10,
+        },
+        {
+          id: 5,
+          name: "Haribo GoldBears",
+          img: (src =
+            "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.haribo.com%2Fen-us%2Fproducts%2Fgoldbears&psig=AOvVaw1Z7sOQevPgWXkHNTuD8Jda&ust=1678889127310000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCLiT6cHL2_0CFQAAAAAdAAAAABAE"),
+          price: 3,
+        },
+        {
+          id: 6,
+          name: "",
+          img: (src = ""),
+          price: 5,
+        },
+        {
+          id: 7,
+          name: "",
+          img: (src = ""),
+          price: 5,
+        },
+        {
+          id: 8,
+          name: "",
+          img: (src = ""),
+          price: 5,
+        },
+        {
+          id: 9,
+          name: "",
+          img: (src = ""),
+          price: 5,
+        },
+        {
+          id: 10,
+          name: "",
+          img: (src = ""),
+          price: 5,
+        },
       ],
     };
   },
