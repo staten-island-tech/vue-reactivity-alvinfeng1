1e95058c470c4cf37c4205eb188147b16b85e4de
Alvin Feng <113121131+alvinfeng1@users.noreply.github.com>
2023-04-03 11:10:53 -0400

    f

--- vite-project/src/components/TastyCards.vue
+++ vite-project/src/components/TastyCards.vue
@@ -18,10 +18,23 @@ export default {
   },
   methods: {
     add() {
-      
-    },
+      let card = cartItems.cart.find((item) => item.name === this.name);
+  if (card) {
+    card.count++;
+    console.log(card.count)
+  } else {
+    cartItems.cart.push({
+      name: this.name,
+      price: this.price,
+      image: this.image,
+      count: 1,
+    });
+    console.log(store.cart)
+  }
+},
   },
-};
+}
+    
 </script>
 
 <style>
